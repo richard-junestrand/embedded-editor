2582e6a44715037f9e4460e524b1617519e70515
Nga Ngo <zorro.lp.x@gmail.com>
2024-10-24 22:38:06 +0700

    Add an icon for opening table of content (and search).

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -5,6 +5,7 @@ import {
 } from '@syncfusion/ej2-react-documenteditor';
 import { useSharedState } from '../state';
 import { MenuItemModel } from '@syncfusion/ej2-react-navigations';
+import $ from 'jquery';
 
 // Inject the Toolbar and ContextMenu dependencies
 DocumentEditorContainerComponent.Inject(Toolbar);
@@ -87,6 +88,21 @@ const Editor = ({ active }: { active: boolean }) => {
       //
       const removedToolbarItems = ["LocalClipboard", "RestrictEditing", "FormFields", "UpdateFields", "ContentControl", "XML Mapping"]
       refEditor.current.toolbarItems = refEditor.current.toolbarItems.flatMap((r: any) => removedToolbarItems.includes(r) ? [] : [r])
+      const ele = $(refEditor.current.editorContainer)
+      ele.on('click', '.show-heading', () => {
+        refEditor.current?.documentEditor.showOptionsPane()
+        setTimeout(() => {
+          const e = ele.find('.e-de-op:visible .e-tab-header .e-toolbar-item[data-id="tabitem_0"]')
+          e.trigger('click');       
+        }, 500);
+      });
+      setTimeout(() => {
+        const e = ele.find('.e-de-pane >.e-de-prop-pane:visible .e-tab-header')
+        const b = e.find('.show-heading')
+        if (b.length === 0) {
+          e.prepend('<button class="btn btn-sm btn-secondary show-heading"><i class="bi bi-card-heading"></i></button>')          
+        }
+      }, 500);
     }
     // eslint-disable-next-line
   }, []);
